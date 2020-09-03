9ff2c2031da3e0b6e7a4e4a52607000b05d31911
chris ye <yechenglong@shein.com>
2020-09-03 19:30:23 +0800

    改用七牛云

--- js/main.js
+++ js/main.js
@@ -432,6 +432,73 @@ right_div.onclick = function () {
         },500)
     }
 }
+//
+// window.onload = function() {
+//     var lineDiv = document.getElementById('timeUl'); //长线条
+//     var ifBool = false; //判断鼠标是否按下
+//     //事件
+//     var start = function(e) {
+//         e.stopPropagation();
+//         ifBool = true;
+//         console.log("鼠标按下")
+//     }
+//     var move = function(e) {
+//         console.log("鼠标拖动")
+//         if(ifBool) {
+//             if(!e.touches) {    //兼容移动端
+//                 var x = e.clientX;
+//             } else {     //兼容PC端
+//                 var x = e.touches[0].pageX;
+//             }
+//             //var x = e.touches[0].pageX || e.clientX; //鼠标横坐标var x
+//             var lineDiv_left = getPosition(lineDiv).left; //长线条的横坐标
+//             //设置拖动后小方块的left值
+//             minDiv.style.left = minDiv_left + "px";
+//             msg.innerText = parseInt((minDiv_left / (lineDiv.offsetWidth - 15)) * 100);
+//             vals.innerText = parseInt((minDiv_left / (lineDiv.offsetWidth - 15)) * 100);
+//
+//             abc((minDiv_left / (lineDiv.offsetWidth - 15)));
+//
+//         }
+//     }
+//     var end = function(e) {
+//         console.log("鼠标弹起")
+//         ifBool = false;
+//     }
+//     //鼠标按下方块
+// //              minDiv.addEventListener("touchstart", start);
+//     minDiv.addEventListener("mousedown", start);
+//     //拖动
+// //              window.addEventListener("touchmove", move);
+//     window.addEventListener("mousemove", move);
+//     //鼠标松开
+// //              window.addEventListener("touchend", end);
+//     window.addEventListener("mouseup", end);
+//     //获取元素的绝对位置
+//     function getPosition(node) {
+//         var left = node.offsetLeft; //获取元素相对于其父元素的left值var left
+//         var top = node.offsetTop;
+//         current = node.offsetParent; // 取得元素的offsetParent
+//         // 一直循环直到根元素
+//
+//         while(current != null) {
+//             left += current.offsetLeft;
+//             top += current.offsetTop;
+//             current = current.offsetParent;
+//         }
+//         return {
+//             "left": left,
+//             "top": top
+//         };
+//     }
+// }
+// function abc(percent){
+//     var distansR=($('.drag').width()-$(window).width()); //可以右滑的极限
+//     console.log(-parseInt(percent*distansR + "px"));
+//     var oLeft = (-parseInt(percent*distansR))+ "px";
+//     $("#contain").css("left",oLeft);
+//     $("#contain").addClass("ggg");
+// }
 
 //手势事件
 var startY,endY;
